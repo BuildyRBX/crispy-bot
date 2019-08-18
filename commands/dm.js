6f2b68812b0f5e456f663e723c322c78bc7a05d4
ethanlaj <ethanlaj@ericlaj.com>
2019-08-18 19:38:10 -0400

    a

---
+++ commands/dm.js
@@ -0,0 +1,23 @@
+module.exports = {
+	id: 'dm',
+	desc: 'Dms a user with a message',
+	exec: async (call) => {
+		if (call.message.author.id === call.client.ownerID) {
+			let author = call.client.users.get(((call.args[0] || '').match(/\d+/) || [])[0]);
+			if (author != null) {
+				let content = call.message.content.substring(call.prefixUsed.length).trim().substring(4 + call.args[0].length).trim();
+				if (content) {
+					let send = await call.message.guild.fetchMember(author);
+
+					send.send(content)
+						.then(() => call.message.react('✅'))
+						.catch(() => call.message.react('❌'));
+				} else {
+					call.message.channel.send('Invalid content. e.g. `,dm @user noob`');
+				}
+			} else {
+				call.message.channel.send('Invalid content. e.g. `,dm @user noob`');
+			}
+		}
+	}
+};
