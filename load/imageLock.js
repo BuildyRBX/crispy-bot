6876da45f89b9eaea1af5a210c2ca434dfe50315
ethanlaj <ethanlaj@ericlaj.com>
2019-08-19 21:03:45 -0400

    message update

--- load/imageLock.js
+++ load/imageLock.js
@@ -3,12 +3,16 @@ const LINK_REGEXP = new RegExp(/[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}
 module.exports = {
 	id: 'imageLock',
 	exec: async (client) => {
-		client.on('message', (message) => {
+		function check (o, n) {
+			let message = n || o;
+
 			if (message.channel.type === 'dm' || !message.channel.topic || !message.channel.topic.includes('[IMAGE_LOCK]'))
 				return;
 
 			if (!message.attachments.first() && !LINK_REGEXP.test(message.content))
 				return message.delete().then(() => message.author.send('This channel is for images/links only.').catch(() => {}));
-		});
+		}
+		client.on('message', (check));
+		client.on('messageUpdate', (check));
 	}
 };
